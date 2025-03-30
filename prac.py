63f6af852e02bfe909f975f798d5d77255f987e2
l-hjab <daphydaphine247@gmail.com>
2025-03-30 22:10:15 +0300

    List in python

---
+++ prac.py
@@ -0,0 +1,14 @@
+import random
+list=['Backend','frontend','API','Database']
+students={
+    'employee1':[236,'jane',20,'sorting '],
+    'employee2':[299,'john',20,'raw mamterial',56],
+    'employee3':[276,'peninah',23,'Quality Control'],
+}
+age=[22,18,30,32]
+for emp in students:
+    print(emp)
+print(students)
+#random module
+print(random.randint(1,10))
+
